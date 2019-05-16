201c605bc2d9affec8d46b2a76fe1ecc4213f440
jongco <xn1128@gmail.com>
2019-05-16 20:05:51 +0900

    ""

--- scripts/editor.js
+++ scripts/editor.js
@@ -3,11 +3,12 @@ var labelPrinted = document.getElementById("printed");
 
 buttonSubmit.addEventListener("click", sendCode);
 
-var xhrObject = new XMLHttpRequest();
+var userCodeLoaderXhr = new XMLHttpRequest();
+var lectureLoaderXhr = new XMLHttpRequest();
 
 function sendCode(){
-    xhrObject.open("POST", "/request-test");
-    xhrObject.send(document.getElementById("editor").value);
+    userCodeLoaderXhr.open("POST", "/request-test");
+    userCodeLoaderXhr.send(document.getElementById("editor").value);
 }
 
 var count = 0;
@@ -17,7 +18,7 @@ function keyFunction(e){
     //이거 그냥 두세요 테스트용입니다.
 }
 
-xhrObject.onload = function(){
-    console.log(xhrObject.response);
-    labelPrinted.innerHTML = xhrObject.response;
+userCodeLoaderXhr.onload = function(){
+    console.log(userCodeLoaderXhr.response);
+    labelPrinted.innerHTML = userCodeLoaderXhr.response;
 }
